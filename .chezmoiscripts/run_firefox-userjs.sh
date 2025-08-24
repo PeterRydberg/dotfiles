d75a5c7a1d11f0fc3e758e0533463c4b6acf2a83
Peter Rydberg <hmp.rydberg@gmail.com>
2025-08-24 22:28:19 +0200

    Update .chezmoiscripts/firefox-userjs.sh

--- .chezmoiscripts/run_firefox-userjs.sh
+++ .chezmoiscripts/run_firefox-userjs.sh
@@ -1,23 +1,18 @@
 #!/bin/sh
 set -e
 
-# Find the default Firefox profile directory from profiles.ini
-PROFILE_DIR=$(awk -F= '/^\[Profile[^0-9]*\]/{p=0} /Default=1/{p=1} p && /^Path=/{print $2}' ~/.mozilla/firefox/profiles.ini)
+FIREFOX_DIR="$HOME/.mozilla/firefox"
+USERJS="$HOME/.mozilla/firefox/user.js"
 
-# Expand tilde if present
-PROFILE_DIR="${PROFILE_DIR/#\~/$HOME}"
-
-# Bail if nothing found
-if [ -z "$PROFILE_DIR" ]; then
-    echo "Could not find default Firefox profile."
+if [ ! -f "$USERJS" ]; then
+    echo "No $USERJS found"
     exit 1
 fi
 
-# Ensure target exists
-mkdir -p "$HOME/.mozilla/firefox/$PROFILE_DIR"
-
-# Symlink user.js from chezmoi-managed config
-ln -sf "$HOME/.config/firefox/user.js" "$HOME/.mozilla/firefox/$PROFILE_DIR/user.js"
-
-echo "Linked user.js into Firefox profile: $PROFILE_DIR"
+for PROFILE_DIR in "$FIREFOX_DIR"/*.default* "$FIREFOX_DIR"/*.Default*; do
+    if [ -d "$PROFILE_DIR" ]; then
+        ln -sf "$USERJS" "$PROFILE_DIR/user.js"
+        echo "Linked user.js into: $PROFILE_DIR"
+    fi
+done
 
